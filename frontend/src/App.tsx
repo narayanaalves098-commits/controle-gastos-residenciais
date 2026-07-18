b29ca71536434fa1df11d8eaad247fa3cdb82bb7
Narayana Alves <narayanaalves098@gmail.com>
2026-07-18 10:28:20 -0300

    feat: implementa cadastro e listagem de pessoas no frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import type { Pessoa } from "./types/Pessoa";
-import { listarPessoas } from "./services/pessoaService";
+import { listarPessoas, cadastrarPessoa } from "./services/pessoaService";
 
 function App() {
   const [pessoas, setPessoas] = useState<Pessoa[]>([]);
+  const [nome, setNome] = useState("");
+  const [idade, setIdade] = useState(0);
 
   useEffect(() => {
     async function carregarPessoas() {
@@ -14,9 +16,39 @@ function App() {
     carregarPessoas();
   }, []);
 
+  async function salvarPessoa() {
+  await cadastrarPessoa(nome, idade);
+
+  const dados = await listarPessoas();
+  setPessoas(dados);
+
+  setNome("");
+  setIdade(0);
+}
+
   return (
     <div>
       <h1>Controle de Gastos Residenciais</h1>
+
+      <h2>Cadastrar Pessoa</h2>
+
+<input
+  type="text"
+  placeholder="Nome"
+  value={nome}
+  onChange={(e) => setNome(e.target.value)}
+/>
+
+<input
+  type="number"
+  placeholder="Idade"
+  value={idade}
+  onChange={(e) => setIdade(Number(e.target.value))}
+/>
+
+<button onClick={salvarPessoa}>
+  Cadastrar
+</button>
 
       <h2>Lista de Pessoas</h2>
 
--- frontend/src/services/pessoaService.ts
+++ frontend/src/services/pessoaService.ts
@@ -6,3 +6,15 @@ export async function listarPessoas(): Promise<Pessoa[]> {
   const resposta = await axios.get<Pessoa[]>(`${API_URL}/pessoa`);
   return resposta.data;
 }
+
+export async function cadastrarPessoa(
+  nome: string,
+  idade: number
+): Promise<Pessoa> {
+  const resposta = await axios.post<Pessoa>(`${API_URL}/pessoa`, {
+    nome,
+    idade
+  });
+
+  return resposta.data;
+}
